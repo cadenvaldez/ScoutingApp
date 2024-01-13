3c24876aea44b94a89daa2ca9db135c8cb6f058d
Caden Valdez <cadenvaldezrobotics@gmail.com>
2024-01-13 16:23:05 -0500

    Scouting App Prototype v0.2 - Leave Alliance Starting Zone Function Added

--- ScoutingAppPROTOTYPE.py
+++ ScoutingAppPROTOTYPE.py
@@ -11,6 +11,7 @@ from tkinter import ttk
 scoreTotal = 0
 scoreAMP = 0
 scoreSpeaker = 0
+leaveInAuto = False
 
 windows = tk.Tk()
 windows.title("Scouting App PROTOTYPE")
@@ -56,46 +57,56 @@ def addAutoSpeakerScore():
     score.configure(text = "Score Total \n" + str(scoreTotal))
     speakerTotalScore.configure(text = "Speaker Total: " + str(scoreSpeaker))
 
+def toggleLeaveInAuto():
+    global leaveInAuto
+    global scoreTotal
+
+    leaveInAuto = True
+    scoreTotal = scoreTotal + 2
+    score.configure(text = "Score Total \n" + str(scoreTotal))
+    leaveStartYesOrNo.configure(text = "Yes")
+
 robotName = tk.Button(windows,
                     text = "Robot Number Input ->",
                     command = printTeamNumber)
 robotName.grid(column=0, row=0)
-
 robotInput = tk.Text(windows, 
                         height = 1,
                         width = 5,)
 robotInput.grid(column=1, row=0)
-
-robotOutput = tk.Label(windows, text = "")
+robotOutput = tk.Label(windows, text = "Team ____ Scouting Data", font = "Helvetica 12 bold")
 robotOutput.grid(column=3, row=0)
 
 ampScoreTELEButton = tk.Button(windows,
                                text = "TeleAMP Score",
                                command = addAmpScore)
 ampScoreTELEButton.grid(column=0, row=1)
-
 ampScoreAUTOButton = tk.Button(windows,
                                text = "AutoAMP Score",
                                command = addAutoAmpScore)
 ampScoreAUTOButton.grid(column=1, row=1)
-
-ampTotalScore = tk.Label(windows)
+ampTotalScore = tk.Label(windows, text = "Amp Total:")
 ampTotalScore.grid(column=3, row=1)
 
 speakerScoreTELEButton = tk.Button(windows,
                                    text = "TeleSpeaker Score",
                                    command = addSpeakerScore)
 speakerScoreTELEButton.grid(column=0, row=2)
-
 speakerScoreAUTOButton = tk.Button(windows,
                                text = "AutoSpeaker Score",
                                command = addAutoSpeakerScore)
 speakerScoreAUTOButton.grid(column=1, row=2)
-
-speakerTotalScore = tk.Label(windows)
+speakerTotalScore = tk.Label(windows, text = "Speaker Total:")
 speakerTotalScore.grid(column=3, row=2)
 
-score = tk.Label(windows, text = "", font = "Helvetica 10 bold")
+leaveStartArea = tk.Button(windows,
+                           text = "Left Start-Area?",
+                           command = toggleLeaveInAuto)
+leaveStartArea.grid(column=1,row=3)
+leaveStartYesOrNo = tk.Label(windows, text = "No")
+leaveStartYesOrNo.grid(column=3,row=3)
+
+score = tk.Label(windows, text = "Score Total \n 0", font = "Helvetica 10 bold")
 score.grid(column=3, row=5)
 
 spacer = tk.Label(windows, text = "                             ")
